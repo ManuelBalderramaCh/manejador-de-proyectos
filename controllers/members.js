5ab358b5796975d30ce8409c7fbf806bc3f75c93
Newty19 <a334144@uach.mx>
2023-03-15 12:11:12 -0600

    Deleted all the overload of files, leaving only the necessary.

--- controllers/members.js
+++ controllers/members.js
@@ -1,83 +1,31 @@
 const express = require('express');
-const { Member } = require('../db');
 
-function list(req, res, next){
-    Member.findAll()
-          .then(objects => res.json(objects))
-          .catch(err => res.send(error));
+function list(req, res, next) {
+    res.send('Lista de miembros');
 }
 
 function index(req, res, next) {
-    const id = req.params.id;
-    Member.findByPk(id)
-             .then(object => res.json(object))
-             .catch(err => res.send(error));
- }
-
- function create(req, res, next){
-    let name = req.body.name;
-    let birthDate = req.body.birthDate;
-    let curp = req.body.curp;
-    let rfc = req.body.rfc;
-    let address = req.body.address;
-    let abilities = req.body.abilities;
-
-    let member = new Object({
-        name:name,
-        birthDate:birthDate,
-        curp:curp,
-        rfc:rfc,
-        address:address,
-        abilities:abilities
-    });
-
-    Member.create(member)
-          .then( obj => res.json(obj))
-          .catch( err => res.json(err));
- }
-
- function replace(req, res, next){
-    const id = req.params.id;
-    Member.findByPk(id)
-          .then((object) => {
-            const name = req.body.name ? req.body.name: "";
-            const birthDate = req.body.birthDate ? req.body.birthDate: "";
-            const curp = req.body.curp ? req.body.curp: "";
-            const rfc = req.body.rfc ? req.body.rfc: "";
-            const address = req.body.address ? req.body.address: "";
-            const abilities = req.body.abilities ? req.body.abilities: "";
-            object.update({name: name, birthDate: birthDate, curp: curp, rfc: rfc, address: address, abilities: abilities})
-                  .then(obj => res.json(obj))
-                  .catch(err => res.send(err));
-          })
-          .catch(err => res.send(err));
- }
-
- function update(req, res, next){
-    const id = req.params.id;
-    Member.findByPk(id)
-          .then((object) => {
-            const name = req.body.name ? req.body.name: object.name;
-            const birthDate = req.body.birthDate ? req.body.birthDate: object.birthDate;
-            const curp = req.body.curp ? req.body.curp: object.curp;
-            const rfc = req.body.rfc ? req.body.rfc: object.rfc;
-            const address = req.body.address ? req.body.address: object.address;
-            const abilities = req.body.abilities ? req.body.abilities: object.abilities;
-            object.update({name: name, birthDate: birthDate, curp: curp, rfc: rfc, address: address, abilities: abilities})
-                  .then(obj => res.json(obj))
-                  .catch(err => res.send(err));
-          })
-          .catch(err => res.send(err));
- }
-
- function destroy(req, res, next) {
-    const id = req.params.id;
-    Member.destroy({ where: {id:id}})
-            .then(obj => res.json(obj))
-            .catch(err => res.send(err));
+    res.send(`Mostrar el miembro numero:  ${req.params.id}`);
 }
 
-module.exports = {
+function create(req, res, next) {
+    let name = req.body.name;
+    res.send(`Registrar al miembro: ${name}`);
+}
+
+function replace(req, res, next) {
+    res.send(`Remplazar al miembro:  ${req.params.id}`);
+}
+
+function update(req, res, next) {
+    res.send(`Modificar al miembro:  ${req.params.id}`);
+}
+
+function destroy(req, res, next) {
+    res.send(`Eliminar al miembro: ${req.params.id}`);
+}
+
+module.exports = { 
     list,
     index,
     create,
--- controllers/projects.js
+++ controllers/projects.js
@@ -1,103 +1,31 @@
 const express = require('express');
-const { Project } = require('../db');
 
-function list(req, res, next){
-    Project.findAll()
-          .then(objects => res.json(objects))
-          .catch(err => res.send(error));
+function list(req, res, next) {
+    res.send('Lista de proyectos');
 }
 
 function index(req, res, next) {
-    const id = req.params.id;
-    Project.findByPk(id)
-             .then(object => res.json(object))
-             .catch(err => res.send(error));
- }
-
- function create(req, res, next){
-    let nameProject = req.body.nameProject;
-    let applicationDate = req.body.applicationDate;
-    let startDate = req.body.startDate;
-    let description = req.body.description;
-    let projectManager = req.body.projectManager;
-    let productOwner = req.body.productOwner;
-    let team = req.body.team;
-
-    let project = new Object({
-        nameProject:nameProject,
-        applicationDate:applicationDate,
-        startDate:startDate,
-        description:description,
-        projectManager:projectManager,
-        productOwner:productOwner,
-        team:team
-    });
-
-    Project.create(project)
-          .then( obj => res.json(obj))
-          .catch( err => res.json(err));
- }
-
- function replace(req, res, next){
-    const id = req.params.id;
-    Project.findByPk(id)
-          .then((object) => {
-            const nameProject = req.body.nameProject ? req.body.nameProject: "";
-            const applicationDate = req.body.applicationDate ? req.body.applicationDate: "";
-            const startDate = req.body.startDate ? req.body.startDate: "";
-            const description = req.body.description ? req.body.description: "";
-            const projectManager = req.body.projectManager ? req.body.projectManager: "";
-            const productOwner = req.body.productOwner ? req.body.productOwner: "";
-            const team = req.body.team ? req.body.team: "";
-            object.update({
-                nameProject:nameProject,
-                applicationDate:applicationDate,
-                startDate:startDate,
-                description:description,
-                projectManager:projectManager,
-                productOwner:productOwner,
-                team:team
-            })
-                  .then(obj => res.json(obj))
-                  .catch(err => res.send(err));
-          })
-          .catch(err => res.send(err));
- }
-
- function update(req, res, next){
-    const id = req.params.id;
-    Project.findByPk(id)
-          .then((object) => {
-            const nameProject = req.body.nameProject ? req.body.nameProject: object.nameProject;
-            const applicationDate = req.body.applicationDate ? req.body.applicationDate: object.applicationDate;
-            const startDate = req.body.startDate ? req.body.startDate: object.startDate;
-            const description = req.body.description ? req.body.description: object.description;
-            const projectManager = req.body.projectManager ? req.body.projectManager: object.projectManager;
-            const productOwner = req.body.productOwner ? req.body.productOwner: object.productOwner;
-            const team = req.body.team ? req.body.team: object.team;
-            object.update({
-                nameProject:nameProject,
-                applicationDate:applicationDate,
-                startDate:startDate,
-                description:description,
-                projectManager:projectManager,
-                productOwner:productOwner,
-                team:team
-            })
-                  .then(obj => res.json(obj))
-                  .catch(err => res.send(err));
-          })
-          .catch(err => res.send(err));
- }
-
- function destroy(req, res, next) {
-    const id = req.params.id;
-    Project.destroy({ where: {id:id}})
-            .then(obj => res.json(obj))
-            .catch(err => res.send(err));
+    res.send(`Mostrar el proyecto numero:  ${req.params.id}`);
 }
 
-module.exports = {
+function create(req, res, next) {
+    let name = req.body.name;
+    res.send(`Registrar al proyecto: ${name}`);
+}
+
+function replace(req, res, next) {
+    res.send(`Remplazar al proyecto:  ${req.params.id}`);
+}
+
+function update(req, res, next) {
+    res.send(`Modificar al proyecto:  ${req.params.id}`);
+}
+
+function destroy(req, res, next) {
+    res.send(`Eliminar al proyecto: ${req.params.id}`);
+}
+
+module.exports = { 
     list,
     index,
     create,
